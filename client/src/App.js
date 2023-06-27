53b754295b5af52f0cdb7cf8b4e4a5ef76ce907d
Chris-Quan <chrisquan86@outlook.com>
2023-06-27 08:31:58 -0400

    added new chat btn and input box

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import './normal.css'
 
 function App() {
   return (
     <div className="App">
       <aside className = "sidemenu">
-        <h1>aside</h1>
+        <div className = "side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
       </aside>
       <section className = "chatbox">
-        <h1>section</h1>
+        <div className = "chat-input-holder">
+          <textarea rows = "1" className = "chat-input-textarea" placeholder = "Type your message here">
+            
+          </textarea>
+        </div>
       </section>
 
     </div>
