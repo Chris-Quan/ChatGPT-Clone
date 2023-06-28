db455a101351836a1dc85d45fdbe0495d31c6479
Chris-Quan <chrisquan86@outlook.com>
2023-06-28 04:55:13 -0400

    you can now communicate with chatgpt api and get a response

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,17 @@ function App() {
     e.preventDefault();
     setChatLog([...chatLog, {user: "me", message: `${input}`}])
     setInput("");
+    const response = await fetch("http://localhost:3080/", {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: chatLog.map((message) => message.message).join("")
+    })
+    });
+    const data = await response.json();
+    setChatLog([...chatLog, {user: "gpt", message: `${data.message}`}])
   }
 
   return (
